c9307b33aef6d3348af969d3ab90c883f96651d0
alzh-azin <86341903+alzh-azin@users.noreply.github.com>
2022-09-13 12:46:13 +0430

    Compose article

--- app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.learnjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learnjetpackcompose.ui.theme.LearnJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ComposeArticle()
                 }
             }
         }
@@ -30,14 +37,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ComposeArticle() {
+
+    val image = painterResource(R.drawable.compose_article)
+
+    Column {
+        Image(
+            image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth().wrapContentHeight(Alignment.Top)
+        )
+        Text(
+            text = stringResource(R.string.compose_article_title),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.compose_article_description),
+            modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.tutorial_description),
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ComposeArticlePreview() {
     LearnJetpackComposeTheme {
-        Greeting("Android")
+        ComposeArticle()
     }
 }
