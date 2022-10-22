bcec456af3026f6283c2912f2d0eb74e00afddb0
alzh-azin <86341903+alzh-azin@users.noreply.github.com>
2022-10-22 19:34:33 +0330

    Jetpack Compose Starter Tutorial (#8)

--- app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
@@ -1,28 +1,40 @@
 package com.example.learnjetpackcompose
 
+import SampleData
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.learnjetpackcompose.ui.theme.LearnJetpackComposeTheme
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LearnJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(Modifier.fillMaxSize()) {
+                    Conversation(messages = SampleData.conversationSample)
                 }
             }
         }
@@ -30,14 +42,72 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Conversation(messages: List<Message>) {
+
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message = message)
+        }
+    }
 }
 
-@Preview(showBackground = true )
+@Composable
+fun MessageCard(message: Message) {
+    Row(Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(
+                R.drawable.girl_avatar
+            ),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(
+                    width = 1.5.dp, color = MaterialTheme.colors.primary, CircleShape
+                )
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+
+        val surfaceColor by animateColorAsState(
+            if (isExpanded)
+                MaterialTheme.colors.primary
+            else
+                MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable {
+            isExpanded = !isExpanded
+        }) {
+            Text(
+                message.author,
+                color = MaterialTheme.colors.secondary,
+                style = MaterialTheme.typography.subtitle1
+            )
+            Spacer(Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 2.dp, color = surfaceColor) {
+                Text(
+                    message.body,
+                    style = MaterialTheme.typography.subtitle2,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    modifier = Modifier.padding(all = 8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     LearnJetpackComposeTheme {
-        Greeting("Android")
+        Surface {
+            Conversation(messages = SampleData.conversationSample)
+        }
     }
 }
