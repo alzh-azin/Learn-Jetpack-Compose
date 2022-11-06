a1708b806368dcd2eeccf08383b4ba2886aa4fd5
alzh-azin <86341903+alzh-azin@users.noreply.github.com>
2022-11-06 12:25:39 +0330

    Write your first compose app (#9)

--- app/src/main/java/com/example/learnjetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/learnjetpackcompose/ui/theme/Theme.kt
@@ -1,50 +1,5 @@
 package com.example.learnjetpackcompose.ui.theme
 
-//import androidx.compose.foundation.isSystemInDarkTheme
-//import androidx.compose.material3.darkColorScheme
-//
-//import androidx.compose.runtime.Composable
-//
-//private val DarkColorPalette = darkColorScheme(
-//    primary = Purple200,
-//    primaryVariant = Purple700,
-//    secondary = Teal200
-//)
-//
-//private val LightColorPalette = lightColors(
-//    primary = Purple500,
-//    primaryVariant = Purple700,
-//    secondary = Teal200
-//
-//    /* Other default colors to override
-//    background = Color.White,
-//    surface = Color.White,
-//    onPrimary = Color.White,
-//    onSecondary = Color.Black,
-//    onBackground = Color.Black,
-//    onSurface = Color.Black,
-//    */
-//)
-//
-//@Composable
-//fun LearnJetpackComposeTheme(
-//    darkTheme: Boolean = isSystemInDarkTheme(),
-//    content: @Composable () -> Unit
-//) {
-//    val colors = if (darkTheme) {
-//        DarkColorPalette
-//    } else {
-//        LightColorPalette
-//    }
-//
-//    MaterialTheme(
-//        colors = colors,
-//        typography = Typography,
-//        shapes = Shapes,
-//        content = content
-//    )
-//}
-
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -65,7 +20,7 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
