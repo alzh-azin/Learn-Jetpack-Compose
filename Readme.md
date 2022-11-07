ffaca9a36d80c229a785ba2df8ca57544568e349
alzh-azin <86341903+alzh-azin@users.noreply.github.com>
2022-11-07 12:52:03 +0330

    Basic layouts in compose (#11)

---
+++ Readme.md
@@ -0,0 +1,64 @@
+# Basic layouts in Compose
+
+In general, to align composables inside a parent container, you set the **alignment** of that parent container. So instead of telling the child to position itself in its parent, you tell the parent how to align its children.
+
+For a `Column`, you decide how its children should be aligned horizontally. The options are:
+
+- Start
+- CenterHorizontally
+- End
+
+For a `Row`, you set the vertical alignment. The options are similar to those of the `Column`:
+
+- Top
+- CenterVertically
+- Bottom
+
+For a `Box`, you combine both horizontal and vertical alignment. The options are:
+
+- TopStart
+- TopCenter
+- TopEnd
+- CenterStart
+- Center
+- CenterEnd
+- BottomStart
+- BottomCenter
+- BottomEnd
+
+All of the container's children will follow this same alignment pattern. You can override the behavior of a single child by adding an [`align`](https://developer.android.com/reference/kotlin/androidx/compose/foundation/layout/ColumnScope#(androidx.compose.ui.Modifier).align(androidx.compose.ui.Alignment.Horizontal)) modifier to it.
+
+For a `Row`, you can choose the following arrangements:
+
+![](readme%20resources\1.PNG)
+
+And for a `Column`:
+
+![](readme%20resources\2.PNG)
+
+In addition to these arrangements, you can also use the `Arrangement.spacedBy()` method to add a fixed space in between each child composable.2
+
+- To maintain the same padding, but still scroll your content within the bounds of your parent list without clipping it, all lists provide a parameter called `contentPadding`.
+
+## Slot-based layouts
+
+Material components make heavy use of *slot APIs*, a pattern Compose introduces to bring in a layer of customization on top of composables. This approach makes components more flexible, as they accept a child element which can configure itself rather than having to expose every configuration parameter of the child. Slots leave an empty space in the UI for the developer to fill as they wish. For example, these are the slots that you can customize in a [`TopAppBar`](https://material.io/components/app-bars-top):
+
+Composables usually take a `content` composable lambda ( `content: @Composable () -> Unit`). Slot APIs expose multiple `content` parameters for specific uses. For example, `TopAppBar` allows you to provide the content for `title`, `navigationIcon`, and `actions`.
+
+For example, [`Scaffold`](https://developer.android.com/reference/kotlin/androidx/compose/material/package-summary#Scaffold(androidx.compose.ui.Modifier,androidx.compose.material.ScaffoldState,kotlin.Function0,kotlin.Function0,kotlin.Function1,kotlin.Function0,androidx.compose.material.FabPosition,kotlin.Boolean,kotlin.Function1,kotlin.Boolean,androidx.compose.ui.graphics.Shape,androidx.compose.ui.unit.Dp,androidx.compose.ui.graphics.Color,androidx.compose.ui.graphics.Color,androidx.compose.ui.graphics.Color,androidx.compose.ui.graphics.Color,androidx.compose.ui.graphics.Color,kotlin.Function1)) allows you to implement a UI with the basic Material Design layout structure. `Scaffold`provides slots for the most common top-level Material components, such as [`TopAppBar`](https://material.io/components/app-bars-top), [`BottomAppBar`](https://material.io/components/app-bars-bottom/), [`FloatingActionButton`](https://material.io/components/buttons-floating-action-button/), and [`Drawer`](https://material.io/components/navigation-drawer/). By using `Scaffold`, it's easy to make sure these components are properly positioned and work together correctly.
+
+```kotlin
+@Composable
+fun HomeScreen(/*...*/) {
+    Scaffold(
+        drawerContent = { /*...*/ },
+        topBar = { /*...*/ },
+        content = { /*...*/ }
+    )
+}
+```
+
+### Scrolling
+
+In general, **you use a Lazy layout when you have many elements in a list or large data sets to load**, so emitting all items at once would come at a performance cost and would slow down your app. When a list has only a limited number of elements, you can instead choose to use a simple `Column` or `Row` and **add the scroll behavior manually**.
--- app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
@@ -3,41 +3,308 @@ package com.example.learnjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.learnjetpackcompose.ui.theme.LearnJetpackComposeTheme
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LearnJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MySootheApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true )
-@Composable
-fun DefaultPreview() {
+fun MySootheApp() {
     LearnJetpackComposeTheme {
-        Greeting("Android")
+        Scaffold(
+            bottomBar = { SootheBottomNavigation() }
+        ) { padding ->
+            HomeScreen(Modifier.padding(padding))
+        }
     }
 }
+
+@Composable
+fun SootheBottomNavigation(modifier: Modifier = Modifier) {
+
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.background,
+        modifier = modifier
+    ) {
+        BottomNavigationItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(stringResource(R.string.bottom_navigation_home))
+            },
+            selected = true,
+            onClick = {}
+        )
+
+        BottomNavigationItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(stringResource(R.string.bottom_navigation_profile))
+            },
+            selected = false,
+            onClick = {}
+        )
+
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+
+    Column(modifier) {
+        Spacer(Modifier.height(16.dp))
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+        HomeSection(title = R.string.favorite_collections) {
+            FavoriteCollectionsGrid()
+        }
+        Spacer(Modifier.height(16.dp))
+    }
+}
+
+
+@Composable
+fun HomeSection(
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(modifier) {
+        Text(
+            text = stringResource(title).uppercase(Locale.getDefault()),
+            style = MaterialTheme.typography.body2,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 8.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
+}
+
+@Composable
+fun FavoriteCollectionsGrid(modifier: Modifier = Modifier) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        modifier = modifier.height(120.dp),
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollectionCard(drawable = item.drawable, text = item.text)
+        }
+    }
+}
+
+@Composable
+fun AlignYourBodyRow(modifier: Modifier = Modifier) {
+
+    LazyRow(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        items(items = alignYourBodyData) { item ->
+            AlignYourBodyElement(drawable = item.drawable, text = item.text)
+        }
+    }
+}
+
+@Composable
+fun FavoriteCollectionCard(
+    modifier: Modifier = Modifier,
+    @DrawableRes drawable: Int,
+    @StringRes text: Int
+) {
+
+    Surface(shape = MaterialTheme.shapes.small) {
+        Row(
+            modifier = modifier.size(192.dp, 56.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(drawable),
+                contentDescription = null,
+                modifier = modifier.width(56.dp),
+                contentScale = ContentScale.FillBounds
+            )
+            Text(
+                text = stringResource(text),
+                style = MaterialTheme.typography.body2,
+                modifier = modifier.padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun AlignYourBodyElement(
+    modifier: Modifier = Modifier,
+    @DrawableRes drawable: Int,
+    @StringRes text: Int
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .clip(CircleShape)
+                .size(88.dp)
+        )
+        Spacer(modifier.height(8.dp))
+        Text(
+            text = stringResource(text),
+            style = MaterialTheme.typography.subtitle2
+        )
+    }
+}
+
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(
+                56.dp
+            ),
+        leadingIcon = {
+            Icon(Icons.Default.Search, contentDescription = null)
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AlignYourBodyElementPreview() {
+    LearnJetpackComposeTheme {
+        AlignYourBodyElement(drawable = R.drawable.ab1_inversions, text = R.string.ab1_inversions)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AlignYourBodyRowPreview() {
+    LearnJetpackComposeTheme {
+        AlignYourBodyRow()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchBarPreview() {
+    LearnJetpackComposeTheme {
+        SearchBar()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FavoriteCollectionCardPreview() {
+    LearnJetpackComposeTheme {
+        FavoriteCollectionCard(
+            text = R.string.fc2_nature_meditations,
+            drawable = R.drawable.fc2_nature_meditations,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FavoriteCollectionsGridPreview() {
+    LearnJetpackComposeTheme() {
+        FavoriteCollectionsGrid()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    LearnJetpackComposeTheme {
+        HomeScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SootheBottomNavigationPreview(){
+    LearnJetpackComposeTheme {
+        SootheBottomNavigation()
+    }
+}
+
+private val alignYourBodyData = listOf(
+    R.drawable.ab1_inversions to R.string.ab1_inversions,
+    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+    R.drawable.ab3_stretching to R.string.ab3_stretching,
+    R.drawable.ab4_tabata to R.string.ab4_tabata,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
+).map { DrawableStringPair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
+
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
